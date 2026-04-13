8f3238848f6bf7cdf1fa2549abefedd4aaeaf833
Remyyy <theremyyy@gmail.com>
2026-04-13 06:27:47 +0200

    Update title to 'System Programming Language'

--- web/pages/index/+config.ts
+++ web/pages/index/+config.ts
@@ -1,6 +1,6 @@
 import { SITE_DESCRIPTION, SITE_NAME } from '../../src/lib/site';
 
 export default {
-    title: `${SITE_NAME} |  Native Programming Language`,
+    title: `${SITE_NAME} |  System Programming Language`,
     description: SITE_DESCRIPTION,
 };
